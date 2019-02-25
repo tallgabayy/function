f55c84e005c32f21a177d2447efa211f844ed380
tallgabayy <47865241+tallgabayy@users.noreply.github.com>
2019-02-25 19:17:23 +0200

    Create functionpractice.js

---
+++ functionpractice.js
@@ -0,0 +1,50 @@
+
+//פונקציה שמציגה מה שבסוגריים
+function hwd ()
+{
+console.log ("hello world")
+}
+hwd ()
+hwd ()
+
+//פונקציה שמחשבת ממוצע
+
+function average (a,b,c)
+{
+  
+ var d = ( (a+b+c)/3 )
+
+console.log (d)
+
+
+}
+
+average (10,8,3)
+average (5,5,5)
+
+
+//  פונקציה של היפוך מינוס/פלוס למספרים
+
+function flip (a,b)
+{
+var f = (a= (-a))
+var f2 = (b= (-b))
+
+alert ("your 1st number flipped to" )
+console.log (f)
+
+alert ("your 2nd number flipped to") 
+console.log (f2)
+
+}
+
+var n = prompt ("number")
+var n2 = prompt ("another one")
+flip (n,n2)
+
+
+
+
+
+
+
